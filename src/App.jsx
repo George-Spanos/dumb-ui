109aac82a6881f92a7799cc99be2dae228c14368
George Spanos <gespa11019@gmail.com>
2022-12-04 21:40:43 +0200

    change url 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { lazy } from "solid-js";
 const Greeting = lazy(async () => {
-  const url = "https://dumb-api.azurewebsites.net/";
+  const url = "https://dumb-api.azurewebsites.net";
   const result = await fetch(url + "/greet").then((r) => r.json());
   console.log(result);
   return <h3>Hello</h3>;
