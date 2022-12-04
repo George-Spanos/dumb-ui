26b3e1dd2eee207fac17e3c32d24b8d933658a15
George Spanos <gespa11019@gmail.com>
2022-12-04 17:09:18 +0200

    change api route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { lazy } from "solid-js";
 const Greeting = lazy(async () => {
-  const result = await fetch("https://dumb-api.azurewebsites.net/greet").then(
+  const result = await fetch("api/greet").then(
     (r) => r.json()
   );
   console.log(result);
